dc2e57291fffa6316ddc8f5cf06008e1d677b502
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2020-06-16 15:09:49 +0300

    update

--- app/Http/Controllers/Modules/TNParseController.php
+++ app/Http/Controllers/Modules/TNParseController.php
@@ -22,53 +22,39 @@ class TNParseController extends Controller
 
     public function upload(Request $request)
     {
-
-
-        $result = array();
-
         if ($request->ajax() && $request->isMethod('post')) {
 
             if ($request->hasFile('file')) {
 
-                $file = $request->file('file');
-                $file_puth = $file->move(public_path('uploads'), time() . '_' . $file->getClientOriginalName());
+                $file1 = $request->file('file_1');
+                $file2 = $request->file('file_2');
+                $file3 = $request->file('file_3');
+                $file4 = $request->file('file_4');
+                //$file_puth = $file->move(public_path('uploads'), time() . '_' . $file->getClientOriginalName());
 
-                $handle = fopen($file_puth, 'r');
+                $handle = fopen($file1, 'r');
+                $result_file_1 = array();
+                $counter = 0;
                 while (($data = fgetcsv($handle, '', '|')) !== FALSE) {
+                    $counter++;
+                    if ($counter == 1) {
+                        continue;
+                    }
 
-//print_r($data);
-                    if (isset($data[1]))
-                        if (mb_detect_encoding($data[1], 'auto') != 'UTF-8') {
-                            $data[1] = iconv('cp866', 'UTF-8', trim($data[1]));
+                    foreach ($data as $key => $value) {
+                        if (mb_detect_encoding($value, 'auto') != 'UTF-8') {
+                            $data[$key] = iconv('cp866', 'UTF-8', $value);
                         }
-
-
-
-
-                    if (isset($data[2]))
-                        if (mb_detect_encoding($data[2], 'auto') != 'UTF-8') {
-                            $data[2] = iconv('cp866', 'UTF-8', trim($data[2]));
-                        }
-                    if (isset($data[3]))
-                        if (mb_detect_encoding($data[3], 'auto') != 'UTF-8') {
-                            $data[3] = iconv('cp866', 'UTF-8', trim($data[3]));
-                        }
-                    if (isset($data[4]))
-                        if (mb_detect_encoding($data[4], 'auto') != 'UTF-8') {
-                            $data[4] = iconv('cp866', 'UTF-8', trim($data[4]));
-                        }
-                    if (isset($data[5]))
-                        if (mb_detect_encoding($data[5], 'auto') != 'UTF-8') {
-                            $data[5] = iconv('cp866', 'UTF-8', trim($data[5]));
-                        }
+                    }
 
                     echo "<strong style='color: #83a6f7;'>" . $data[0] . "</strong>   <strong>" . $data[1] . "</strong>";
-                    echo "<p style='color: #a3a3a5;border-bottom: 1px Dashed #e6e6e6;padding: 0 0 1rem;'>" . $data[2] . $data[3] ."</p>";
-
+                    echo "<p style='color: #a3a3a5;border-bottom: 1px Dashed #e6e6e6;padding: 0 0 1rem;'>" . $data[2] . $data[3] . "</p>";
+                    array_pop($data);
+                    $result_file_1[] = $data;
                 }
 
                 fclose($handle);
-
+                dd($result_file_1);
             }
 
         } else {
